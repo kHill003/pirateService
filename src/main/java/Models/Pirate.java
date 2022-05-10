84274adaa31445788bb0cf5932c10bf61c78359c
kHill003 <kris.hillquist@codeclan.com>
2022-05-10 10:34:15 +0100

    addecd a class

---
+++ src/main/java/Models/Pirate.java
@@ -0,0 +1,57 @@
+// Pirate.java
+import javax.persistence.Entity; // NEW
+import javax.persistence.Table; // NEW
+
+@Entity // NEW
+@Table(name="pirates") // NEW
+
+
+public class Pirate {
+
+    private Long id; // NEW
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Pirate(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Pirate() { // NEW
+
+    }
+
+    public Long getId() { // NEW
+        return id;
+    }
+
+    public void setId(Long id) { // NEW
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
